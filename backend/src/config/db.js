a2a27be757193754cdd2ad4bc49669744add7b90
Luis Angel Ojeda <119816563+LuisAOL2003@users.noreply.github.com>
2026-02-26 13:38:35 -0400

    Update db.js

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -5,11 +5,11 @@ import pkg from 'pg';
 const { Pool } = pkg;
 
 const pool = new Pool({
-  host: process.env.DB_HOST || 'localhost',
-  port: process.env.DB_PORT || 5432,
-  user: process.env.DB_USER || 'postgres',
-  password: process.env.DB_PASSWORD || '17102003',
-  database: process.env.DB_NAME || 'Proyecto_Cines'
+  host: process.env.DB_HOST ,
+  port: process.env.DB_PORT ,
+  user: process.env.DB_USER ',
+  password: process.env.DB_PASSWORD ',
+  database: process.env.DB_NAME'
 });
 
 export default pool;
